72aa052d48b1ec654b26fd1e442c2e50c56e2fe9
Ryokuman <ryokuman@naver.com>
2024-06-18 22:23:13 +0900

    chore : src/index
     - add provider for redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./stores/store";
 import "@assets/css/reset.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
