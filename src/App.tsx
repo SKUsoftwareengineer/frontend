5d17191d97041d6efae4c595877811a16ad117d0
Ryokuman <ryokuman@naver.com>
2024-06-18 21:59:04 +0900

    feature : src/App
    feature : Basic
    feature : Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import { Main } from "@pages";
+import { Header } from "@components";
+import styled from "styled-components";
+
+const Basic = styled.div`
+  width: 50vw;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: flex-start;
+
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Main />} />
-    </Routes>
+    <Basic>
+      <Header />
+      <Routes>
+        <Route index element={<Main />} />
+      </Routes>
+    </Basic>
   );
 }
 
