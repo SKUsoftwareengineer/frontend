f4cc4cab92ee46032c31e7c4d949016ad1717c20
Ryokuman <ryokuman@naver.com>
2024-06-18 22:16:39 +0900

    chore : src/pages/main/index
    feature : Link
    - add link to the button

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,13 @@
 import { Button, Body } from "@components";
+import { Link } from "react-router-dom";
 
 function Main() {
   return (
     <Body>
       <h1>미용실 키오스크</h1>
-      <Button margin="300px 0 20px 0">언어선택</Button>
+      <Link to="lang">
+        <Button margin="300px 0 20px 0">언어선택</Button>
+      </Link>
       <Button margin="20px">예약하기</Button>
     </Body>
   );
