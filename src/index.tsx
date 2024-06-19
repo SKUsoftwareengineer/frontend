07313045e367476042efc9c6fd64bf127440a036
Ryokuman <ryokuman@naver.com>
2024-06-20 00:12:01 +0900

    chore : pacakage
    delete : react-calendar
    feature : mui

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./stores/store";
-import "react-calendar/dist/Calendar.css";
-import "@assets/css/reset.css";
-import "@assets/css/calendar.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
