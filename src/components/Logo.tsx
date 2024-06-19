ad5e501f5f3f1ecb26ba0e7272446d43f6e3db69
Ryokuman <ryokuman@naver.com>
2024-06-19 20:56:02 +0900

    chore : components
    chore : Logo
     - add link to Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,27 @@
 import styled from "styled-components";
 import logoPath from "../assets/img/Logo.jpeg";
+import { Link } from "react-router-dom";
 
-const Logo = styled.div`
+const IMG = styled.div`
+  width: 200px;
+  height: 200px;
+  background-image: url(${logoPath});
+  background-size: cover;
+`;
+
+const Body = styled(Link)`
   width: 200px;
   height: 200px;
   position: absolute;
   top: 100px;
-
-  background-image: url(${logoPath});
-  background-size: cover;
 `;
+
+function Logo() {
+  return (
+    <Body to="../">
+      <IMG />
+    </Body>
+  );
+}
 
 export { Logo };
