06febfbf01476a2263fcbb73406d3e8767159b7a
Ryokuman <ryokuman@naver.com>
2024-06-19 20:30:55 +0900

    feature : stores
    feature : stores/bookSlice
     - add bookSlice to redux for control booking informations
    
    chore : store/store
     - add bookSlice to redux

--- src/stores/store.tsx
+++ src/stores/store.tsx
@@ -1,12 +1,13 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import { langSlice } from "./langSlice";
+import { bookSlice } from "./bookSlice";
 
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
 
 const store = configureStore({
-  reducer: { lang: langSlice.reducer },
+  reducer: { lang: langSlice.reducer, book: bookSlice.reducer },
 });
 
 const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
