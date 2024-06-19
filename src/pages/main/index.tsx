67e00e8a4f6f531ea2eca66684dcbf97760d1ac5
Ryokuman <ryokuman@naver.com>
2024-06-19 20:29:52 +0900

    chore : pages
    chore : pages/lang
    chore : pages/main
     - add Logo

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Body } from "@components";
+import { Button, Body, Logo } from "@components";
 import { Link } from "react-router-dom";
 import texts from "@assets/json/main.json";
 import { useAppSelector } from "@stores/store";
@@ -8,7 +8,7 @@ function Main() {
 
   return (
     <Body>
-      <h1>미용실 키오스크</h1>
+      <Logo />
       <Link to="lang">
         <Button margin="300px 0 20px 0">{texts[lang].langBtn}</Button>
       </Link>
